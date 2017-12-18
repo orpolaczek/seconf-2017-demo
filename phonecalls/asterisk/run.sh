6d086dd0a804a50005a0b34c8e2f2b686fd08b97
Or Polaczek <or.polaczek@forter.com>
2017-12-18 18:47:05 +0200

    Minor script improvements

--- phonecalls/asterisk/run.sh
+++ phonecalls/asterisk/run.sh
@@ -2,4 +2,5 @@ docker run \
     --net=host \
     -v $(pwd)/asterisk-example-conf/:/etc/asterisk/ \
     -v $(pwd)/../recordings/:/tmp/recordings/ \
+    -v $(pwd)/../../../astricon-2017-demos/mfa/:/home/scripts/mfa/ \
     -d -t seconf2017-asterisk
